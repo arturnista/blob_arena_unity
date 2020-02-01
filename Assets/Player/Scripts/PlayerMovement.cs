187df0c5d8f96aef979ca9d6d3b4b9bf4937ba4b
artur nista <arturnista@gmail.com>
2020-02-01 05:35:11 -0300

    AAAAAAAAAAAAAAAA

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.03f;
+    
     public InputSchema Schema;
 
     public float MoveSpeed;
     public float Acceleration;
     public float JumpHeight;
+    public float AirDrag;
     public LayerMask GroundMask;
 
     private float jumpForce;
@@ -17,25 +21,28 @@ public class PlayerMovement : MonoBehaviour
 
     private new Rigidbody2D rigidbody;
     private float moveDirection;
+    private float lookingDirection;
+    public Vector2 LookingDirection { get => lookingDirection * Vector2.right; }
 
     private float targetSpeed;
-    private Vector2 velocity;
+    private Vector2 moveVelocity;
+    private Vector2 extraVelocity;
+    public Vector2 Velocity { get => moveVelocity + extraVelocity; }
 
     private bool isGrounded;
+    public bool IsGrounded { get => isGrounded; }
+
 
     protected ContactFilter2D contactFilter;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-
-    protected const float minMoveDistance = 0.001f;
-    protected const float shellRadius = 0.03f;
-
-    private Vector2 nextMovement;
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         gravity = Physics2D.gravity;
         jumpForce = Mathf.Sqrt(JumpHeight * 2f * -gravity.y);
+
+        lookingDirection = 1f;
 
         contactFilter.useTriggers = false;
         contactFilter.SetLayerMask (GroundMask);
@@ -45,44 +52,55 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         moveDirection = Input.GetAxisRaw(Schema.HorizontalAxis);
+        if (moveDirection != 0) lookingDirection = moveDirection; 
 
         if (Schema.GetKeyDown(Schema.Jump) && isGrounded) 
         {
-            velocity.y = jumpForce;
+            moveVelocity.y = jumpForce;
             isGrounded = false;
         }
 
         targetSpeed = moveDirection * MoveSpeed;
-        velocity.x = Mathf.MoveTowards(velocity.x, targetSpeed, Acceleration * Time.deltaTime);
+        moveVelocity.x = Mathf.MoveTowards(moveVelocity.x, targetSpeed, Acceleration * Time.deltaTime);
+        extraVelocity.x = Mathf.MoveTowards(extraVelocity.x, 0f, AirDrag * Time.deltaTime);
 
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+        transform.localScale = new Vector3(lookingDirection, 1f, 1f);
     }
 
     void FixedUpdate()
     {   
-        MovePlayer(Time.fixedDeltaTime);
+        Vector2 motion = MovePlayer(Time.fixedDeltaTime);
 
-        rigidbody.MovePosition(rigidbody.position + nextMovement);
-        nextMovement = Vector2.zero;
+        rigidbody.MovePosition(rigidbody.position + motion);
     }
 
-    void MovePlayer(float deltaTime)
+    Vector2 MovePlayer(float deltaTime)
     {
         isGrounded = false;
-        Vector2 deltaPosition = velocity * deltaTime;
+        Vector2 finalVelocity = moveVelocity + extraVelocity;
+        Vector2 deltaPosition = finalVelocity * deltaTime;
 
         Vector2 vMove = Vector2.up * deltaPosition.y;
-        Vector2 vMovement = Movement (vMove);
+        Vector2 vMovement = Movement (vMove, ref moveVelocity);
 
         Vector2 hMove = Vector2.right * deltaPosition.x;
-        Vector2 hMovement = Movement (hMove);
+        Vector2 hMovement = Movement (hMove, ref moveVelocity);
 
-        nextMovement += vMovement + hMovement;
-        
-        velocity += gravity * deltaTime;
-        velocity.y = Mathf.Clamp(velocity.y, -50.0f, float.PositiveInfinity);
+        moveVelocity += gravity * deltaTime;
+        extraVelocity += gravity * deltaTime;
+
+        moveVelocity.y = Mathf.Clamp(moveVelocity.y, -50.0f, float.PositiveInfinity);
+        extraVelocity.y = Mathf.Clamp(extraVelocity.y, -50.0f, float.PositiveInfinity);
+
+        return vMovement + hMovement;
     }
 
-    Vector2 Movement(Vector2 move)
+    Vector2 Movement(Vector2 move, ref Vector2 velocity)
     {
         float distance = move.magnitude;
         if (distance > minMoveDistance) 
@@ -112,5 +130,10 @@ public class PlayerMovement : MonoBehaviour
 
         return move.normalized * distance;
     } 
+
+    public void AddExtraVelocity(Vector2 velocity)
+    {
+        extraVelocity += velocity;
+    }
 
 }
---
+++ Assets/Scripts/Menu/MenuScreenBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MenuScreenBase : MonoBehaviour
+{
+    
+    public delegate void NextScreenHandle();
+    public NextScreenHandle OnNextScreen;
+
+}
