148cb79dc6f27eadbb557395ae5ef25559814711
Henrique <riquesouzalima11@gmail.com>
2020-02-01 06:56:36 -0300

    sla

--- Assets/Player/Scripts/PlayerBag.cs
+++ Assets/Player/Scripts/PlayerBag.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerBag : MonoBehaviour
 {
-    public int itens; 
+    public int itens;
+    public GameObject peca;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,10 @@ public class PlayerBag : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            DropPeca(0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,5 +33,32 @@ public class PlayerBag : MonoBehaviour
                 //win condition
             }
         }
+    }
+
+    private void DropPeca(int side)//0 - esq || 1 - dir
+    {
+        if(itens >0)
+        {
+            if (side == 0)
+            {
+                GameObject help;
+                help = Instantiate(peca, transform.position + transform.up, Quaternion.identity);
+
+                Vector2 vec = new Vector2(Random.Range(-1.0f, 0.0f), Random.Range(0.5f, 1.0f));
+                help.GetComponent<Rigidbody2D>().AddForce(vec * 500);
+
+                itens--;
+            }
+            else
+            {
+                GameObject help;
+                help = Instantiate(peca, transform.position + transform.up, Quaternion.identity);
+                Vector2 vec = new Vector2(Random.Range(0.0f, 1.0f), Random.Range(0.5f, 1.0f));
+                help.GetComponent<Rigidbody2D>().AddForce(vec * 500);
+                itens--;
+
+            }
+        }
+        
     }
 }
---
+++ Assets/Scripts/UI/UIupdate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIupdate : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject[] Pecas;
+
+    private int Npeca;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Npeca = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Npeca < 5)
+        {
+            if (Player.GetComponent<PlayerBag>().itens > Npeca)
+            {
+                Pecas[Npeca].SetActive(true);
+                Npeca++;
+            }
+        }
+        
+    }
+}
