98360d5d2e05be637dd0edba63378afb1ec7ae84
ArturEndress <artur-efo@live.com>
2020-02-01 11:28:40 -0300

    weapon and bullet Scripts

---
+++ Assets/Player/Scripts/weaponScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform weaponPos, spawnBulletPoint;
+    public GameObject bullet;
+    bool isEquiped;
+    void Start()
+    {
+        isEquiped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEquiped)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                
+               Shoot();
+              
+            }
+           
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "weapon")
+        {
+            collision.transform.SetParent(weaponPos.transform);
+            collision.transform.localPosition = Vector2.zero;
+            isEquiped = true;
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(bullet, spawnBulletPoint.position, Quaternion.identity);
+        isEquiped = false;
+        GameObject obj = weaponPos.GetChild(0).gameObject;
+        Destroy(obj);
+        Debug.Log("Destroy");
+        
+    }
+}
---
+++ Assets/bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float spd;
+    bool isFacingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x + spd * Time.deltaTime, transform.position.y);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
