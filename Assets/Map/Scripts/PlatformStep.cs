159f0e0185d2c5bc20bc97a6d96b90e7cb0ecef1
artur nista <arturnista@gmail.com>
2020-02-02 02:17:29 -0300

    Added splash and hit to weapons.

---
+++ Assets/Map/Scripts/PlatformStep.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStep : MonoBehaviour
+{
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            animator.SetTrigger("Step");
+        }
+    }
+
+}
---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+
+    public float Delay;
+
+    void Start()
+    {
+        Destroy(gameObject, Delay);
+    }
+
+}
