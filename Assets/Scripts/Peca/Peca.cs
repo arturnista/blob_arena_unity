ffb69ce851ec12e33022a71a69c7a205fdf40b19
artur nista <arturnista@gmail.com>
2020-02-02 14:41:53 -0300

    Peca

---
+++ Assets/Scripts/Peca/Peca.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peca : MonoBehaviour
+{
+    
+    private new Collider2D collider;
+    void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    public void DisableForTime()
+    {
+        StartCoroutine(DisableCoroutine());
+    }
+
+    IEnumerator DisableCoroutine()
+    {
+        collider.enabled = false;
+        yield return new WaitForSeconds(.2f);
+        collider.enabled = true;
+    }
+
+}
