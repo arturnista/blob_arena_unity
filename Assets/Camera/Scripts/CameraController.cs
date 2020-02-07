30d09c970fc51016c981fc9bfa4056c70e864acb
Artur Morelle Nista <arturnista@gmail.com>
2020-02-06 21:49:38 -0300

    Fixed map camera

---
+++ Assets/Camera/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    private Camera camera;
+    private const float mapSizeOrthographicSize = 40f;
+    private const float minOrthographicSize = 10f;
+
+    void Awake()
+    {
+        camera = GetComponentInChildren<Camera>();
+        float ratio = (float)camera.pixelWidth / camera.pixelHeight;
+        
+        float orthographicSize = mapSizeOrthographicSize / ratio / 2;
+        camera.orthographicSize = Mathf.Clamp(orthographicSize, minOrthographicSize, Mathf.Infinity);
+    }
+
+}
